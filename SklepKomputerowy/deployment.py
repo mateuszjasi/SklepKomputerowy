9af3f460b646f17d62e228a4a5d5bc1624f06d94
Mateusz <mateuszjasi@op.pl>
2023-12-03 16:22:41 +0100

    Azure deployment

--- SklepKomputerowy/deployment.py
+++ SklepKomputerowy/deployment.py
@@ -19,7 +19,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'mainApp/static')
 
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
