0beadda28ace5b0ef19d6c64210efad17e780751
Mateusz <mateuszjasi@op.pl>
2023-12-03 21:23:21 +0100

    Azure deployment

--- SklepKomputerowy/deployment.py
+++ SklepKomputerowy/deployment.py
@@ -25,10 +25,7 @@ conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
