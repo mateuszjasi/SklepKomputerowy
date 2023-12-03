f772e9f289db8423fb38cb3ce3e8435786188bb5
Mateusz <mateuszjasi@op.pl>
2023-12-03 16:42:13 +0100

    Azure deployment

--- SklepKomputerowy/settings.py
+++ SklepKomputerowy/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from django.conf import settings
 import os
 
+from django.contrib import staticfiles
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,6 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mainApp/static'),
